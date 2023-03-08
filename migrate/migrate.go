2762ea5b66e4078deed1a4528bb65d20064ab25f
Ali Karakoç <bykrkc@gmail.com>
2023-03-08 23:00:10 +0300

    migrate added

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,11 +1,14 @@
 package main
 
-import "GoApp/initializers"
+import (
+	"GoApp/initializers"
+	"GoApp/models"
+)
 
 func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDb()
 }
 func main() {
-	initializers.DB.AutoMigrate("&models.Post{}")
+	initializers.DB.AutoMigrate(&models.Post{})
 }
